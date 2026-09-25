9d1deeee96d93c32dc0a8bb6dbbe0b7336d3cda1
GeniaSoftWin / Genia Link / Firewall / Password / Proxy <geniasoftwin@gmail.com>
2026-09-25 22:44:43 +0300

    feat: mark TUIC runtime-verified in Protocol Lab

--- Services/ProtocolLabFeatureCatalog.cs
+++ Services/ProtocolLabFeatureCatalog.cs
@@ -48,8 +48,8 @@ namespace GeniaProxy.Services
                 false,
                 "Alpha 2 / Protocol Lab",
                 ProtocolLabEngineFamily.SingBox,
-                ProtocolLabSupportState.EngineAvailable,
-                false
+                ProtocolLabSupportState.RuntimeVerified,
+                true
             ),
             new(
                 "snell",
